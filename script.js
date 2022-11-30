dae98482135efd40b674ef3e7c93d566af2167ed
masaya_doi <masaya_doi@nnn.ac.jp>
2022-11-30 16:00:26 +0900

    1章想定

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const cards = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 
 const left = document.getElementById("left");
 const right =  document.getElementById("right");
-const resultHtml =  document.getElementById("resultHtml");
-const judgeHtml = document.getElementById("judgeHtml");
 
 
 let leftNum = 0;
@@ -16,41 +14,7 @@ function start(){
 
     left.src = "card/0_" + cards[leftNum] + ".png"; // カードを描画
 
-    judgeHtml.style.visibility = "visible"; //投票ボタンを表示
-
 }
 
-function judge(isHigh) {
-    let result = "";
-
-    if (cards[leftNum] < cards[rightNum]) { // 右のカードの方が大きい
-        if (isHigh === true){ // isHighがtrueなら勝ち
-            result = true;
-        }else{ // isHighがfalseなら負け
-            result = false;
-        }
-    }else{ // 右のカードの方が小さい
-        if (isHigh === true){ // isHighがtrueなら負け
-            result = false;
-        }else{ // isHighがfalseなら勝ち
-            result = true;
-        }
-    }
-
-    showResult(result);
-}
-
-function showResult(result){
-    // 結果を表示
-    right.src = "card/1_" + cards[rightNum] + ".png"; // 右のカードを表示
-    if(result === true) {
-        resultHtml.innerText = "あなたの勝ち！";
-    }else{
-        resultHtml.innerText = "あなたの負け...";
-    }
-    
-    judgeHtml.style.visibility = "hidden"; //投票ボタンを非表示
-
-}
 
 window.onload = start();
